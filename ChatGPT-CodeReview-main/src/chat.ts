a00f6e888b32007d9d895e10c12b94b7d221b0c1
erictsaii <erictsai90@gmail.com>
2023-05-15 17:00:52 +0800

    sec

---
+++ ChatGPT-CodeReview-main/src/chat.ts
@@ -0,0 +1,40 @@
+import { ChatGPTAPI } from 'chatgpt';
+export class Chat {
+  private chatAPI: ChatGPTAPI;
+
+  constructor(apikey: string) {
+    this.chatAPI = new ChatGPTAPI({
+      apiKey: apikey,
+      apiBaseUrl: process.env.OPENAI_API_ENDPOINT || 'https://api.openai.com/v1',
+      completionParams: {
+        model: process.env.MODEL || 'gpt-3.5-turbo',
+        temperature: +(process.env.temperature || 0) || 1,
+        top_p: +(process.env.top_p || 0) || 1,
+      },
+    });
+  }
+
+  private generatePrompt = (patch: string) => {
+    const answerLanguage = process.env.LANGUAGE
+      ? `Answer me in ${process.env.LANGUAGE},`
+      : '';
+
+    return `Bellow is the code patch, please help me do a brief code review,${answerLanguage} if any bug risk and improvement suggestion are welcome
+    ${patch}
+    `;
+  };
+
+  public codeReview = async (patch: string) => {
+    if (!patch) {
+      return '';
+    }
+
+    console.time('code-review cost');
+    const prompt = this.generatePrompt(patch);
+
+    const res = await this.chatAPI.sendMessage(prompt);
+
+    console.timeEnd('code-review cost');
+    return res.text;
+  };
+}
