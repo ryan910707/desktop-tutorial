d2267d9cc2d097e3760a61fae322e0c343589ec9
109062113 <109062113>
2023-05-15 18:11:37 +0800

    fda

--- Bellman-Ford.c
+++ Bellman-Ford.c
@@ -13,13 +13,6 @@ struct Edge
     // This structure is equal to an edge. Edge contains two end points. These edges are directed edges so they
 //contain source and destination and some weight. These 3 are elements in this structure
     int source, destination, weight;
-    char sjfjfa;
-    char fa;
-    char a, b, c;
-    int ssss;
-    int qqqq;
-    int sffff;
-    int fda;
     int asdfaklsdjflkasjdfla;
 };
  
